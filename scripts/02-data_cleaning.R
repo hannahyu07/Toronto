479b87fdb50ff785ab9c5fe381645ccc3196285d
Hannah Yu <hannahyuh07@gmail.com>
2024-01-19 21:33:13 +0000

    trial

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,8 +1,8 @@
 #### Preamble ####
 # Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Author: Hannah Yu
+# Date: 19 January 2023  
+# Contact: realhannah.yu@mail.utoronto.ca 
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
 # Any other information needed? [...UPDATE THIS...]
